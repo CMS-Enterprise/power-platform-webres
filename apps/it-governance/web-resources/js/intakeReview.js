e4ba30725cfc2944c86e99c2d040bb9d6e2a18d6
Cooper Heinrichs <cooper.heinrichs@oddball.io>
2026-04-03 14:35:31 -0600

    updates

--- apps/it-governance/web-resources/js/intakeReview.js
+++ apps/it-governance/web-resources/js/intakeReview.js
@@ -4,63 +4,6 @@ function onLoad(executionContext) {
   updateProgressTracker(formContext);
 }
 
-function onSave(executionContext) {
-  const formContext = executionContext.getFormContext();
-  const COL_LCID_REQUESTED = "cr69a_lcidrequested"; // Two options / Boolean
-  const COL_LCID_REQUESTED_ON = "cr69a_lcidrequestedon"; // DateTime
-  const COL_LCID_REQUESTED_BY = "cr69a_lcidrequestedby"; // Lookup -> systemuser
-
-  if (
-    formContext.getAttribute(COL_LCID_REQUESTED) &&
-    formContext.getAttribute(COL_LCID_REQUESTED).getValue()
-  ) {
-    try {
-      if (!formContext) {
-        setStatus(
-          "Unable to access the form. Please refresh and try again.",
-          true,
-        );
-        return;
-      }
-
-      // Ensure the columns are on the form (even if hidden)
-      const aRequested = formContext.getAttribute(COL_LCID_REQUESTED);
-      const aRequestedOn = formContext.getAttribute(COL_LCID_REQUESTED_ON);
-      const aRequestedBy = formContext.getAttribute(COL_LCID_REQUESTED_BY);
-
-      if (!aRequested || !aRequestedOn || !aRequestedBy) {
-        console.error(
-          "Either aRequested, aRequestedOn, aRequestedBy is not defined",
-          "aRequested",
-          aRequested,
-          "aRequestedOn",
-          aRequestedOn,
-          "aRequestedBy",
-          aRequestedBy,
-        );
-        return;
-      }
-
-      // Set values
-      aRequested.setValue(true);
-      aRequestedOn.setValue(new Date());
-
-      // Lookup value needs [{ id: '{GUID}', name: 'Display Name', entityType: 'systemuser' }]
-      const user = parent.Xrm.Utility.getGlobalContext().userSettings;
-      const userIdWithBraces = `{${user.userId.replace(/[{}]/g, "")}}`;
-      aRequestedBy.setValue([
-        {
-          id: userIdWithBraces,
-          name: user.userName || user.userId,
-          entityType: "systemuser",
-        },
-      ]);
-    } catch (e) {
-      console.error("Unexpected error:", e);
-    }
-  }
-}
-
 function showHideSections(formContext, tabName, fields) {
   for (var key in fields) {
     const section = formContext.ui.tabs.get(tabName).sections.get(key);

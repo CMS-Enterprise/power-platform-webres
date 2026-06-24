2ed1c97175b0770cc0e2a23266664c3e0f4ae338
Wyatt Emme <wyatt.emme@oddball.io>
2026-06-24 13:47:35 -0400

    Update to remove Id

--- apps/it-governance/web-resources/js/edit-request/editRequest.js
+++ apps/it-governance/web-resources/js/edit-request/editRequest.js
@@ -1,15 +1,15 @@
 function hideFieldForUnauthorizedUsers(executionContext) {
     var formContext = executionContext.getFormContext();
-    var userId = Xrm.Utility.getGlobalContext().getUserId();
-    
-    // Check if user has a specific role
-    var userRoles = Xrm.Utility.getGlobalContext().getUserRoles();
-    var allowedRoleId = "4622b038-2217-f111-8407-001dd809993d";
-    
-    var hasRole = userRoles.some(function(roleId) {
-        return roleId === allowedRoleId;
-    });
+    var roles = Xrm.Utility.getGlobalContext().userSettings.roles;
 
-    // Hide the field if they don't have the role
-    formContext.getControl("cr69a_adminnotes").setVisible(hasRole);
+    var roleName = "IT Governance - Admin";
+    
+    var hasRole = roles.get(function(role) {
+        return role.name === roleName;
+    }).length > 0;
+
+    var control = formContext.getControl("cr69a_adminnotes");
+    if (control) {
+        control.setVisible(hasRole);
+    }
 }
